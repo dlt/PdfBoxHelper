39e055f6a2b50f8b0d1f48ce3c641811f86ccedb
Dalto Curvelano Jr <daltojr@gmail.com>
2020-03-12 16:18:24 +0400

    Refactor

--- src/main/scala/PdfHelper.scala
+++ src/main/scala/PdfHelper.scala
@@ -1,4 +1,4 @@
-import org.apache.pdfbox.pdmodel.PDDocument
+import org.apache.pdfbox.pdmodel.{PDDocument, PDDocumentCatalog}
 import java.io.File
 
 import org.apache.pdfbox.pdmodel.interactive.action.PDActionGoTo
@@ -6,65 +6,61 @@ import org.apache.pdfbox.pdmodel.interactive.documentnavigation.destination.{PDN
 import org.apache.pdfbox.pdmodel.interactive.documentnavigation.outline.PDOutlineItem
 
 import scala.collection.mutable
-
+import org.json4s._
+import org.json4s.JsonDSL._
 import org.json4s.native.Json
-import org.json4s.DefaultFormats
 
 object PdfHelper {
-  def main(args: Array[String]): Unit = {
-    val doc = PDDocument.load(new File(args.head))
-    val size = doc.getNumberOfPages
-    val catalog = doc.getDocumentCatalog
-    val outline = catalog.getDocumentOutline
-
-    val titles = new mutable.HashMap[String, Int]
-    val queue = new mutable.Queue[PDOutlineItem]()
-    val visited = new mutable.HashSet[PDOutlineItem]()
-
-    queue.enqueue(outline.getFirstChild)
-    var current = queue.dequeue()
-
-    while (current != null) {
-      if (!visited.contains(current)) {
-        var pd: PDPageDestination = null;
-
-        current.getDestination match {
-          case destination: PDNamedDestination =>
-            pd = catalog.findNamedDestinationPage(destination)
-          case destination: PDPageDestination =>
-            pd = destination
-          case _ => current.getAction match {
-            case gta: PDActionGoTo =>
-              gta.getDestination match {
-                case destination: PDPageDestination =>
-                  pd = destination
-                case _ =>
-              }
+  def getPageNumber(item: PDOutlineItem, catalog: PDDocumentCatalog): Int = {
+    var pd: PDPageDestination = null;
+    item.getDestination match {
+      case destination: PDNamedDestination =>
+        pd = catalog.findNamedDestinationPage(destination)
+      case destination: PDPageDestination =>
+        pd = destination
+      case _ => item.getAction match {
+        case gta: PDActionGoTo =>
+          gta.getDestination match {
+            case destination: PDPageDestination =>
+              pd = destination
             case _ =>
           }
-        }
+        case _ =>
+      }
+    }
+    pd.retrievePageNumber() + 1
+  }
 
-        titles += (current.getTitle -> (pd.retrievePageNumber() + 1))
+  def addAllLeavesToQueue(queue: mutable.Queue[PDOutlineItem], item: PDOutlineItem) {
+    item.children.forEach(c => queue.enqueue(c))
+    var next = item.getNextSibling
+    while (next != null) {
+      queue.enqueue(next)
+      next = next.getNextSibling
+    }
+  }
 
-        current.children().forEach(c => queue.enqueue(c))
-        var next = current.getNextSibling
-        while (next != null) {
-          queue.enqueue(next)
-          next = next.getNextSibling
-        }
+  def getBookmarks(document: PDDocument): List[(String, Int)] = {
+    val catalog = document.getDocumentCatalog
+    val queue = new mutable.Queue[PDOutlineItem]()
+    val visited = new mutable.HashSet[PDOutlineItem]()
+    queue.enqueue(catalog.getDocumentOutline.getFirstChild)
+    var bookmarks: List[(String, Int)] = List()
 
+    while (queue.nonEmpty) {
+      val current = queue.dequeue()
+      if (!visited.contains(current)) {
+        bookmarks = bookmarks :+ (current.getTitle -> getPageNumber(current, catalog))
+        addAllLeavesToQueue(queue, current)
         visited.add(current)
       }
-
-      if (queue.nonEmpty) {
-        current = queue.dequeue()
-      } else {
-        current = null
-      }
-
     }
+    bookmarks
+  }
 
-    println(Json(DefaultFormats).write(titles))
-    println(size.toString)
+  def main(args: Array[String]): Unit = {
+    val doc = PDDocument.load(new File(args.head))
+    val json = ("metadata" -> ("pages" -> doc.getNumberOfPages) ~ ("bookmarks" -> getBookmarks(doc)))
+    println(Json(DefaultFormats).write(json))
   }
 }
